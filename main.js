01d869c4f0a8c54c5dff22048ec5b3edac5a1733
cnqinglin <2207328000@qq.com>
2019-08-24 10:19:57 +0800

    docs

--- main.js
+++ main.js
@@ -50,29 +50,54 @@ setTimeout(function(){
 
 
 //抽象思维实现
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-var n = 1
-setInterval(() => {
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one('transitionend',(e)=>{  //transitionend就是指动画结束以后马上添加其它函数,`xxx(${n})`采用ES6的插值法，n多少那么这一部分就是多少
-        $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
-    n += 1
-},3000)
+let n
+初始化()
 
-function x(n){
-    if(n>3){
-        n = n%3
-        if(n===0){
+setInterval(() => {
+    makeLeave(getImage(n)) //makeLeave的返回值是undefined
+        .one('transitionend', (e) => { //transitionend就是指动画结束以后马上添加其它函数,`xxx(${n})`采用ES6的插值法，n多少那么这一部分就是多少
+            makeEnter($(e.currentTarget))   //给当前元素添加enter状态
+        })
+    makeCurrent(getImage(n + 1))
+    n += 1
+}, 2000)
+
+//下面的代码都是封装之后的函数，可以不用看
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) {
             n = 3
             //n = 1,2,3
-        }   
+        }
     }
     return n
 }
+ 
+//初始化就是让n=1,让第一个变成current，其他的变成enter
+function 初始化() {
+    n = 1               
+    //初始化三个图片
+    $(`.images > img:nth-child(${n}`).addClass('current')
+        .siblings().addClass('enter')
+}
 
+function makeCurrent($node) {
+    return $node.removeClass("enter leave").addClass("current")
+}
+
+function makeEnter($node) {
+    return $node.removeClass("current leave").addClass("enter")
+}
+
+function makeLeave($node) {
+    $node.removeClass("enter current").addClass("leave")
+    return $node //防止返回 undefined,操作一番，传入什么就传出什么
+}
 
 //找规律
 /*
